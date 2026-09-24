7e65ff4cb69f1e5e573ee5bca3a322e1e35a6d80
Miguel060215 <miguelangelortegamartinez00@gmail.com>
2026-09-24 01:27:14 -0600

    Se creo un sistema de Checkpoint para regresar a un punto especifico al morir, se agrego animacion de muerte a otix, al igual que una pequena transicion al reaparecer, tambien se diseno e implemento un sistema de recuperacion de vida en los mismos objetos que guardan el checkpoint.

--- Assets/Scripts/SaludJugador.cs
+++ Assets/Scripts/SaludJugador.cs
@@ -1,6 +1,8 @@
 using UnityEngine;
 using System.Collections;
 using Unity.VisualScripting;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class SaludJugador : MonoBehaviour
 {
@@ -17,6 +19,15 @@ public class SaludJugador : MonoBehaviour
 
     private Player scriptPlayer;
 
+    [Header("Audio")]
+    public AudioSource fuenteEfectos;
+    public AudioClip clipDano;
+    [Header("Secuencia de Muerte")]
+    public AudioSource audioAmbiental;
+    public AudioClip clipMuerte;
+    public Image pantallaNegra;
+    public SeguimientoCamara camaraScript; // Referencia al script de seguimiento de cámara
+
     void Start()
     {
         scriptPlayer = GetComponent<Player>();
@@ -27,7 +38,10 @@ public class SaludJugador : MonoBehaviour
 
     public void RecibirDaño(int cantidadDano)
     {
+        if (esInvencible || saludActual <= 0) return;
+
         if (esInvencible) return;
+        fuenteEfectos.PlayOneShot(clipDano,0.5f);
 
         saludActual -= cantidadDano;
 
@@ -39,9 +53,15 @@ public class SaludJugador : MonoBehaviour
         else StartCoroutine(RutinaInvencibilidad());
     }
 
+    public void CurarAlMaximo() {
+        saludActual = saludMaxima;
+        ActualizarVidasUI();
+    }
+
     public void RecibirDannoTrampa(int cantidadDano)
     {
         if (esInvencible) return;
+        fuenteEfectos.PlayOneShot(clipDano, 0.5f);
 
         // lo regresamos a la ultima zona segura q toco y le blokeamos el movimiento un rato
         Player scriptPlayer = GetComponent<Player>();
@@ -98,7 +118,63 @@ public class SaludJugador : MonoBehaviour
     void Muerte()
     {
         Debug.Log("Otix ha muerto");
-        // Despues programo que es lo que pasara al morir el personaje
+        StartCoroutine(SecuenciaMuerte());
+    }
+
+    private IEnumerator SecuenciaMuerte() {
+        //bloqueo el movimiento del personaje y lo detengo
+        if (scriptPlayer != null) { 
+            scriptPlayer.puedeMoverse = false;
+            Rigidbody2D rb = GetComponent<Rigidbody2D>();
+            if (rb != null) { 
+                rb.linearVelocity = Vector2.zero;
+                rb.gravityScale = 0f; // Evita que el personaje caiga mientras está muerto
+            }
+            Collider2D col = GetComponent<Collider2D>();
+            if (col != null) { 
+                col.enabled = false;
+            }
+        }
+        //hago que se incie la animacoon de muerte
+        Animator anim = GetComponent<Animator>();
+        if (anim != null) { 
+            anim.SetTrigger("Muerte");
+        }
+        //apago la musica de fondo del nivel y pongo la de muerte de Otix
+        if (audioAmbiental != null) {
+            audioAmbiental.Stop();
+        }
+        if (scriptPlayer != null && clipMuerte != null)
+        {
+            scriptPlayer.fuenteCaminar.Stop();
+            scriptPlayer.fuenteCaminar.clip = clipMuerte;
+            scriptPlayer.fuenteCaminar.loop = true;
+            scriptPlayer.fuenteCaminar.Play();
+        }
+        yield return new WaitForSeconds(3f);
+
+        //se acerca la caamara al personaje muerto
+        if (camaraScript != null) { 
+            camaraScript.IniciarZoom(3f, 3f); //Si quiero ajujstar el zoom y la duracion, puedo cambiar los valores aqui
+        }
+        yield return new WaitForSeconds(3f);
+
+        //se oscurece la pantalla
+        if (pantallaNegra != null)
+        {
+            float tiempo = 0;
+            float duracionFade = 1.5f;
+            Color colorBase = pantallaNegra.color;
+
+            while (tiempo < duracionFade) { 
+                tiempo += Time.deltaTime;
+                float alfa = Mathf.Lerp(0, 1, tiempo / duracionFade);
+                pantallaNegra.color = new Color(colorBase.r, colorBase.g, colorBase.b, alfa);
+                yield return null;
+            }
+        }
+        //reinicio la escena
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
     // hace q el perosnaje parpadee y no reciba daño por un tiempo
@@ -112,7 +188,7 @@ public class SaludJugador : MonoBehaviour
 
         float tiempoPasado = 0f;
 
-        // un siklo q cambia el color a transparente y normal rapido
+        // un siclo q cambia el color a transparente y normal rapido
         while (tiempoPasado < tiempoInvencibilidad)
         {
             spriteRenderer.color = colorTransparente;
